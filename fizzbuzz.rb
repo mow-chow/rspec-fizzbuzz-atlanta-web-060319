aeed3c017a1d0ad402d0d68c3530a447822ff091
Catherine Cherubin <cat.cherubin@gmail.com>
2019-05-07 17:05:42 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,10 @@
 # See README.md for instructions on how to do this
 def fizzbuzz
   if int % 3 && int % 5 == 0
-    "FizzBuzz" 
+    "FizzBuzz"
   elsif int % 3 == 0
     "Fizz"
   elsif int % 5 == 0
     "Buzz"
-
-
   end
 end
